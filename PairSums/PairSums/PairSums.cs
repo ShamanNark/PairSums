42b88357be58e36a2a604499e65957b32239a94b
ShamanNark <7301878@gmail.com>
2016-09-20 00:00:51 +0300

    Find pair by sums

---
+++ PairSums/PairSums/PairSums.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairSums
+{
+    public static class PairSums
+    {
+        public static void printPairSums(int[] array, int sum)
+        {
+            Array.Sort(array);
+            int first = 0;
+            int last = array.Length - 1;
+            while (first < last)
+            {
+                int s = array[first] + array[last];
+
+                if (s == sum)
+                {
+                    Console.WriteLine(array[first] + " " + array[last]);
+                    int pred_first = first;
+                    int pred_last = last;
+
+                    do
+                    {
+                        first++;
+                        last--;
+
+                    } while (array[first] == array[first - 1] && array[last] == array[last + 1] && (first < last));
+                    
+                }
+                else
+                {
+                    if (s < sum) first++;
+                    else last--;
+                }
+            }
+        }
+    }
+}
---
+++ PairSums/PairSums/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PairSums
+{
+    class Program
+    {
+        void Main(string[] args)
+        {
+            int[] array = { -2, -1, 0, 3, 5, 6, 7, 9, 13, 14 };
+            PairSums.printPairSums(array, 12);
+            Console.ReadKey();
+        }
+    }
+}
